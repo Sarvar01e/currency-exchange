4a5a4d2e16d7bf6d22d34f00ec1d3e196df1e330
user <user@WIN-AKM2LNADFM7>
2022-11-19 20:47:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace currency_exchange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сurrency exchanger\n USD to BYN course is 2.39");
+            Console.WriteLine("Enter your value");
+            string str;
+
+            str = Console.ReadLine();
+
+            double a = Convert.ToDouble(str);
+
+            double exchange = a * 2.39;
+
+            Console.WriteLine("Your money in BYN \t" + exchange);
+
+
+        }
+    }
+}
